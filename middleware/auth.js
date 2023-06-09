3ee9892e64794e94bb00ca39fef9ab6ad6353b13
Deepak Singh <56964147+dk-cell@users.noreply.github.com>
2023-06-09 14:29:44 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,6 +6,7 @@ const Shop = require("../model/shop");
 
 exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
+  console.log("------>isAuth", req)
 
   if (!token) {
     return next(new ErrorHandler("Please login to continue", 401));
