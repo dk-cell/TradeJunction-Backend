fd397a086c977376015751ce849b2b07d6ff1f6f
dk-cell <singhdpk009@gmail.com>
2023-06-18 22:42:19 +0530

    fixed token issue

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const Shop = require("../model/shop");
 
 exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const bearer = req.headers.authorization;
-  const token = bearer.split(' ')[1]
+  const token = bearer.split(" ")[1];
   if (!token) {
     return next(new ErrorHandler("Please login to continue", 401));
   }
@@ -18,9 +18,8 @@ exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.isSeller = catchAsyncErrors(async (req, res, next) => {
-
   const bearer = req.headers.authorization;
-  const seller_token = bearer.split(' ')[2]
+  const seller_token = bearer.split(" ")[2];
   if (!seller_token) {
     return next(new ErrorHandler("Please login to continue", 401));
   }
